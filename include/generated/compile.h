eaa70e790381f78b314d47abef514c57093ca79e
jenkins <jenkins@teamw.in>
2015-11-05 20:19:30 -0500

    initial push

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Oct 18 18:32:48 EDT 2015"
+#define LINUX_COMPILE_BY "jenkins"
+#define LINUX_COMPILE_HOST "build"
+#define LINUX_COMPILER "gcc version 4.9 20140514 (prerelease) (GCC) "
